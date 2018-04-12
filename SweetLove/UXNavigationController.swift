11fa6aa13cdc50d92a725620bd0e1b93dbad0517
hieutc2 <hieutc2@vng.com.vn>
2018-04-12 09:06:42 +0700

    Add navigation controller

---
+++ SweetLove/UXNavigationController.swift
@@ -0,0 +1,14 @@
+//
+//  UXNavigationController.swift
+//  SweetLove
+//
+//  Created by hieutc2 on 4/12/18.
+//  Copyright © 2018 Chi-Hieu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class UXNavigationController : UINavigationController {
+    
+}
